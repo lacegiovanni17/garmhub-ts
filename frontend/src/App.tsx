40709c4fa61910b80fdf2432b0054d2406088c4b
Chidike Henry <dk1online25@gmail.com>
2023-04-11 21:40:52 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>Garm-Hub</header>
+      <main></main>
+      <footer>Copyright © Garm-Hub. All rights reserved. Made with ❤️ in Lagos🇳🇬.</footer>
+    </div>
+  )
+}
+
+export default App
