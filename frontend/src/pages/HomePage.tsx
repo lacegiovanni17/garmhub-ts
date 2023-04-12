a49ef87765a23d771632d7ecee13e926c22348ef
Chidike Henry <dk1online25@gmail.com>
2023-04-13 00:57:06 +0100

    added new feat

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,87 +1,75 @@
 import { Row, Col } from "react-bootstrap";
-import { sampleProducts } from "../data";
-import { Link } from "react-router-dom";
 import { Product } from "../types/Product";
 import { useEffect, useReducer } from "react";
 import { ApiError } from "../types/ApiError";
-import axios from "axios";
 import { getError } from "../utils";
+import axios from "axios";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import ProductItem from "../components/ProductItem";
 
 type State = {
-    products: Product[],
-    loading: boolean
-    error: string
-}
+  products: Product[];
+  loading: boolean;
+  error: string;
+};
 
 type Action =
-|{ type: 'FETCH_REQUEST' }
-|{
-  type: 'FETCH_SUCCESS'
-  payload: Product[]
-} 
-|{ type: 'FETCH_FAIL'; payload: string }
-
+  | { type: "FETCH_REQUEST" }
+  | {
+      type: "FETCH_SUCCESS";
+      payload: Product[];
+    }
+  | { type: "FETCH_FAIL"; payload: string };
 
 const initialState: State = {
-    products: [],
-    loading: true,
-    error: '',
-}
+  products: [],
+  loading: true,
+  error: "",
+};
 
 const reducer = (state: State, action: Action) => {
-    switch (action.type) {
-        case 'FETCH_REQUEST':
-            return { ...state, loading: true }
-        case 'FETCH_SUCCESS':
-            return { ...state, products: action.payload, loading: false }
-        case 'FETCH_FAIL':
-            return { ...state, loading: false, error: action.payload }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default function HomePage() {
-    const [{ loading, error, products }, dispatch] = useReducer<
-        React.Reducer<State, Action>
-    >(reducer, initialState)
+  const [{ loading, error, products }, dispatch] = useReducer<
+    React.Reducer<State, Action>
+  >(reducer, initialState);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            dispatch({ type: 'FETCH_REQUEST' })
-            try {
-                const result = await axios.get('/api/products')
-                dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
-            } catch (err) {
-                dispatch({ type: 'FETCH_FAIL', payload: getError(err as ApiError) })
-            }
-        }
-        fetchData()
-    }, [])
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err as ApiError) });
+      }
+    };
+    fetchData();
+  }, []);
 
-    return (
-        loading ? (
-            <LoadingBox />
-        ) : error ? (
-                <MessageBox variant='danger'>{error}</MessageBox>
-      ): (
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
     <Row>
-      {sampleProducts.map((product) => (
+      {products.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
-          <Link to={"/product/" + product.slug}>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            />
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </Link>
+          <ProductItem product={product} />
         </Col>
       ))}
     </Row>
-    )
   );
 }
