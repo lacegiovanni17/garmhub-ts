9301a62c522cf9b812c6311f84ef471d5298c2b3
Chidike Henry <dk1online25@gmail.com>
2023-04-12 22:14:18 +0100

    added new feat

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,6 +13,8 @@ import './index.css'
 import HomePage from './pages/Homepage'
 import ProductPage from './pages/ProductPage';
 
+axios.defaults.baseURL =
+process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
