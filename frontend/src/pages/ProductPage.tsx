a09f3c820858ec5a4971cf399be03b8b4d54552c
Chidike Henry <dk1online25@gmail.com>
2023-04-13 12:55:19 +0100

    added title of page

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 export default function ProductPage() {
   return (
-    <div>ProductPage</div>
+    <div>
+      <Helmet>
+        <title>Product Page</title>
+      </Helmet>
+      Product Page
+    </div>
   )
 }
